a12bcf9bdf95fa145034b877929b56c0ed7cb0b0
Winds-of-luck <91137167+Winds-of-luck@users.noreply.github.com>
2021-10-05 14:19:07 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,69 @@
+
+let start = 0
+let end = 0
+let false_start = false
+let running = false
+input.onPinPressed(TouchPin.P0, () => {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    running = false
+    false_start = false
+    basic.pause(1000 + randint(0, 2000))
+    if (!(false_start)) {
+        start = input.runningTime()
+        running = true
+        led.stopAnimation()
+        basic.clearScreen()
+        led.plot(randint(0, 4), randint(0, 4))
+    }
+})
+input.onPinPressed(TouchPin.P1, () => {
+    if (running) {
+        running = false
+        end = input.runningTime()
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(1000)
+        basic.showNumber(end - start)
+    } else {
+        false_start = true
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            . # . . .
+            # . # . .
+            . . . . .
+            `)
+    }
+})
+input.onPinPressed(TouchPin.P2, () => {
+    if (running) {
+        running = false
+        end = input.runningTime()
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        basic.pause(1000)
+        basic.showNumber(end - start)
+    } else {
+        false_start = true
+        basic.showLeds(`
+            . . . . .
+            . . # . #
+            . . . # .
+            . . # . #
+            . . . . .
+            `)
+    }
+})
